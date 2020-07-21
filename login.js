c209c70f419485582c3fde722dbf66c4343124d8
shinyo1357 <shinyo1357@gmail.com>
2020-07-21 23:23:32 +0800

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,13 @@
 function readtext() {
     var account = document.getElementById("account").value;
     var password = document.getElementById("password").value;
-    alert(account+password);
+    
+    if (account == "shinyogood"&&password == "2580369b")
+      {
+        location.href = "https://www.google.com/"
+      }
+    else
+      {
+        location.href = "https://www.google.com/"
+      }
 }
