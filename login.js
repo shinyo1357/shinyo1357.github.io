43dfc3ebb42c1f348100c28ae2d987966c4e3f7c
shinyo1357 <shinyo1357@gmail.com>
2020-07-23 12:24:39 +0800

    up

--- login.js
+++ login.js
@@ -1,13 +1,13 @@
 function readtext() {
     var account = document.getElementById("account").value;
     var password = document.getElementById("password").value;
-    
-    if (account == "shinyogood"&&password == "2580369b")
+
+    if (account == "shinyogood" && password == "2580369b")
       {
-        window.location.href='http://www.google.com';
+        window.location.href= "https://www.google.com/" ;
       }
     else
       {
-        alert("您的帳號或密碼輸入錯咯!")
+        alert("您的帳號或密碼輸入錯咯!");
       }
 }
