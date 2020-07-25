dc20541570ea613133fba9146a8fe4f664ca4e49
shinyo1357 <shinyo1357@gmail.com>
2020-07-25 16:53:03 +0800

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ function readtext() {
     var account = document.getElementById("account").value;
     var password = document.getElementById("password").value;
 
-    if (account == "shinyogood" && password == "2580369b")
+    if ((account == "shinyogood" && password == "2580369b") || (account == "123" && password == "123"))
       {
         document.location.href="http://www.google.com";
       }
