faabf7a2ac0ef4dfaf315f12cf22996ddbe569d1
shinyo1357 <shinyo1357@gmail.com>
2020-07-28 20:29:15 +0800

    123

--- login.js
+++ login.js
@@ -1,10 +1,11 @@
 function readtext() {
     var account = document.getElementById("account").value;
     var password = document.getElementById("password").value;
+    var req = new XMLHttpRequest();
 
     if ((account == "shinyogood" && password == "2580369b") || (account == "123" && password == "123"))
       {
-        document.location.href="http://www.google.com";
+        req.open("get", shinyo1357.github.io/home.html)
       }
     else
       {
