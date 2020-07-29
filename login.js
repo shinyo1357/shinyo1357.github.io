90a9838984452d9a35c6667c8952a722351570d9
shinyo1357 <shinyo1357@gmail.com>
2020-07-29 20:47:54 +0800

    Update login.js

--- login.js
+++ login.js
@@ -4,7 +4,7 @@ function readtext() {
 
     if ((account == "shinyogood" && password == "2580369b") || (account == "123" && password == "123"))
       {
-        document.getElementById("form").innerHTML = '<input type="button" value="123">';
+        document.getElementById("form").innerHTML = '<a href="/%E5%9C%96%E7%89%87%E8%B3%87%E6%96%99/3.png" download>Download the picture</a>';
       }
     else
       {
