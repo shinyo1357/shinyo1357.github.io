50b9204f745a06418342fc14bdf1e8d96423cebf
shinyo1357 <shinyo1357@gmail.com>
2020-07-31 20:24:00 +0800

    Update login.js

--- login.js
+++ login.js
@@ -4,7 +4,7 @@ function readtext() {
 
     if ((account == "shinyogood" && password == "2580369b") || (account == "123" && password == "123"))
       {
-        document.getElementById("form").innerHTML = '<a href="https://drive.google.com/file/d/1WJx1OGBGpFxSWOENK8r2rZL7_oAWfeaJ/view?usp=sharing" download="OOF.png"><button>快按我!</button></a>';
+        document.getElementById("form").innerHTML = '<a href="/%E5%9C%96%E7%89%87%E8%B3%87%E6%96%99/3.png" download="OOF.png"><button>快按我!</button></a>';
       }
     else
       {
